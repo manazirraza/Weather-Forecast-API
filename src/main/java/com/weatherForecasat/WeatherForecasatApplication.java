f7f12a7e522b9d0abea28ac5a95e8cefe8b26d61
Manazir Raza <manazirraza1999@gmail.com>
2026-02-28 14:34:34 +0530

    new

--- src/main/java/com/weatherForecasat/WeatherForecasatApplication.java
+++ src/main/java/com/weatherForecasat/WeatherForecasatApplication.java
@@ -1,8 +1,16 @@
 package com.weatherForecasat;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.concurrent.TimeUnit;
+
+@EnableCaching
 @SpringBootApplication
 public class WeatherForecasatApplication {
 
@@ -10,4 +18,24 @@ public class WeatherForecasatApplication {
 		SpringApplication.run(WeatherForecasatApplication.class, args);
 	}
 
+	@Bean
+	public WebClient webClient() {
+		return WebClient.builder()
+				.baseUrl("https://api.openweathermap.org")
+				.build();
+	}
+
+	@Bean
+	public CaffeineCacheManager cacheManager() {
+		CaffeineCacheManager cacheManager =
+				new CaffeineCacheManager("weatherCache");
+
+		cacheManager.setCaffeine(
+				Caffeine.newBuilder()
+						.expireAfterWrite(10, TimeUnit.MINUTES) // TTL
+						.maximumSize(100)                      // max entries
+		);
+
+		return cacheManager;
+	}
 }
--- src/main/java/com/weatherForecasat/service/WeatherService.java
+++ src/main/java/com/weatherForecasat/service/WeatherService.java
@@ -1,19 +1,31 @@
 package com.weatherForecasat.service;
 
 import com.weatherForecasat.payloads.WeatherDto;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
 @Service
 public class WeatherService {
 
+    private WebClient webClient;
     private final String api_key="0894b69454e212ea6bd582fc2f5ae283";
 
+    public WeatherService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    @Cacheable(value = "weatherCache", key = "#city")
     public WeatherDto getWether(String city) {
+        System.out.println("Open api calling");
+        WeatherApiResponseDto response =webClient
+                .get()
+                .uri("/data/2.5/weather?q={city}&appid={key}", city, api_key)
+                .retrieve()
+                .bodyToMono(WeatherApiResponseDto.class)
+                .block();
 
-        String url ="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key;
-
-        RestTemplate restTemplate = new RestTemplate();
-        WeatherApiResponseDto response = restTemplate.getForObject(url, WeatherApiResponseDto.class);
 
         return new WeatherDto(
                 response.getName(),
